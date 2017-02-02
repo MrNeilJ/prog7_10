23924618a583c2290d8b1e812f673f0e825753eb
Neil Johnson <mr.neil.j@gmail.com>
2017-02-02 01:10:11 -0800

    Added main function Added a second function

--- main.cpp
+++ main.cpp
@@ -60,3 +60,36 @@ int main()
     showValues(part);
     return 0;
 }
+
+/*****************************************
+*           createItem                   *
+*   This function stores user input data *
+*   in the members of a locally define   *
+*   InventoryItem object, then returns it*
+*****************************************/
+
+InventoryItem createItem()
+{
+    InventoryItem tempItem;         // Local InventoryItem object
+    int partNum;                    // Local variables to hold user input
+    string description;
+    int qty;
+    double price;
+
+    // Get the data from the user
+    cout << "Enter data for the new part number \n";
+    cout << "Part number: ";
+    cin >> partNum;
+    cout << "Description: ";
+    cin.get();                      // Move past the '\n' left in the
+                                    // input buffer by the last input
+    getline(cin, description);
+    cout << "Quantity on hand: ";
+    cin >> qty;
+    cout << "Unit price: ";
+    cin >> price;
+
+    // Store the data in the InventoryItem object and return it
+    tempItem.storeInfo(partNum, description, qty, price);
+    return tempItem;
+}
